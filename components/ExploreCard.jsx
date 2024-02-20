9961099d4f29b19ead38cf46b5fb702f6d083ba7
papichaa <parmida.shoeibzade4@gmail.com>
2024-02-20 15:07:27 +0300

    New update

--- components/ExploreCard.jsx
+++ components/ExploreCard.jsx
@@ -8,13 +8,13 @@ const ExploreCard = ({ active, index, id, imgUrl, title, handleClick }) => (
   <motion.div
     variants={fadeIn('right', 'spring', index * 0.5, 0.75)}
     className={`relative ${active === id ? 'lg:flex-[3.5] flex-[10]' : 'lg:flex-[0.5] flex-[2]'}
-  flex items-center justify-center min-w-[170px] h-[700px] transition-[flex] duration-[0.7s]
+  flex items-center justify-center min-w-[170px] h-[400px] transition-[flex] duration-[0.7s]
   ease-out-flex cursor-pointer`}
     onClick={() => handleClick(id)}
   >
     <img src={imgUrl} alt={title} className="absolute w-full h-full object-cover rounded-[24px]" />
     {active !== id ? (
-      <h3 className="font-semibold sm:text-[26px] text-[18px] text-white absolute z-0 lg:bottom-20 lg:rotate-[-90deg] lg:origin-[0,0]">
+      <h3 className="font-semibold sm:text-[26px] text-[18px] text-white absolute z-0 lg:bottom-10 lg:rotate-[-90deg] lg:origin-[0,0]">
         {title}
       </h3>
     ) : (
--- constants/index.js
+++ constants/index.js
@@ -6,23 +6,23 @@ export const exploreWorlds = [
   },
   {
     id: 'world-2',
-    imgUrl: '/planet-02.png',
-    title: 'The Upside Down',
+    imgUrl: '/Evently.png',
+    title: 'Evently_ecommerce',
   },
   {
     id: 'world-3',
-    imgUrl: '/planet-03.png',
-    title: 'Kadirojo Permai',
+    imgUrl: '/travel_app.png',
+    title: 'Travel_app',
   },
   {
     id: 'world-4',
-    imgUrl: '/planet-04.png',
-    title: 'Paradise Island',
+    imgUrl: '/promptopia.png',
+    title: 'Promptopia',
   },
   {
     id: 'world-5',
-    imgUrl: '/planet-05.png',
-    title: 'Hawkins Labs',
+    imgUrl: '/blog.png',
+    title: 'Blog',
   },
 ];
 
